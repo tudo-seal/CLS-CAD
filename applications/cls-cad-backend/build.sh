6e57542970e3d329c5f94fbd49249cce8671e6ad
Constantin Chaumet <constantin.chaumet@tu-dortmund.de>
2023-08-21 12:30:28 +0200

    Update dependencies and add lock file

--- applications/cls-cad-backend/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-poetry run nuitka --standalone --onefile --include-module=cls_cps.server --include-module=montydb.storage.flatfile --include-package=dns --assume-yes-for-downloads --output-dir=build --enable-plugin=tk-inter --include-data-dir=cls_cps/static=cls_cps/static -o clscps.bin main.py
